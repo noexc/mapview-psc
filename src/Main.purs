1504d226ce45dc339c4530aabe3d58c04743bb54
Ricky Elrod <ricky@elrod.me>
2014-05-26 00:53:50 -0400

    Some design and some more psc functions
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Main.purs
+++ src/Main.purs
@@ -19,6 +19,58 @@ foreign import getElementById
   \  };\
   \}" :: forall eff. String -> Eff eff Element
 
+foreign import setOnclick
+  "function setOnclick(ele) {\
+  \  return function(f) {\
+  \    return function() {\
+  \      ele.onclick = f;\
+  \      return;\
+  \    };\
+  \  };\
+  \}" :: forall eff a. Element -> (a -> Unit) -> Eff eff Unit
+
+foreign import setDisplayFn
+  "function setDisplayFn(ele) {\
+  \  return function(s) {\
+  \    return function() {\
+  \      return function() {\
+  \        ele.style.display = s;\
+  \        return;\
+  \      };\
+  \    };\
+  \  };\
+  \}" :: forall eff. Element -> String -> Eff eff (Unit -> Unit)
+
+foreign import setDisplay
+  "function setDisplay(ele) {\
+  \  return function(s) {\
+  \    return function() {\
+  \      ele.style.display = s;\
+  \      return;\
+  \    };\
+  \  };\
+  \}" :: forall eff. Element -> String -> Eff eff Unit
+
+foreign import setInnerHtml
+  "function setInnerHtml(ele) {\
+  \  return function(s) {\
+  \    return function() {\
+  \      ele.innerHTML = s;\
+  \      return;\
+  \    };\
+  \  };\
+  \}" :: forall eff. Element -> String -> Eff eff (Unit -> Unit)
+
+foreign import setClass
+  "function setClass(ele) {\
+  \  return function(s) {\
+  \    return function() {\
+  \      ele.className = s;\
+  \      return;\
+  \    };\
+  \  };\
+  \}" :: forall eff. Element -> String -> Eff eff (Unit -> Unit)
+
 data MapOptions = MapOptions {
     zoom :: Number
   , center :: LatLng
@@ -55,8 +107,24 @@ foreign import panTo
   \  };\
   \}" :: forall eff. Map -> LatLng -> Eff eff Unit
 
+setDismissAnnouncement :: forall eff. Eff eff Unit
+setDismissAnnouncement = do
+  dismissButton <- getElementById "dismiss_announcement"
+  announcement <- getElementById "announcement"
+  y <- setDisplayFn announcement "none"
+  setOnclick dismissButton y
 
+setAnnouncement :: forall eff. String -> String -> Eff eff Unit
+setAnnouncement t c = do
+  announcement <- getElementById "announcement"
+  text <- getElementById "announcement_text"
+  setInnerHtml text t
+  setClass announcement c
+  setDisplay announcement "block"
+
+main :: Eff (trace :: Trace) {}
 main = do
+  setDismissAnnouncement
   mapE <- getElementById "map-canvas"
   startingPoint <- newLatLng 0.0 0.0
   roadmap <- gMap mapE (MapOptions { zoom: 6, center: startingPoint, mapTypeId: "roadmap" })
